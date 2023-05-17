694fbb0a3739dad124164b6105b76a0bbf627989
tiyee <tiyee@live.com>
2023-05-17 23:46:03 +0800

    feat: v2.0.0

---
+++ dist/wxm/wxm.d.ts
@@ -0,0 +1,27 @@
+/** @format */
+/// <reference types="wechat-miniprogram" />
+import { IFileHandle, IRequest, IURLSearchParams, Uploader } from 'uploader';
+import { IInitRet, IContext, IUploadPartRet } from 'uploader';
+declare class URLSearchParams implements IURLSearchParams {
+    private data;
+    constructor();
+    set(k: string, v: string): void;
+    toString(): string;
+}
+declare class WxmFile implements IFileHandle {
+    readonly fsm: WechatMiniprogram.FileSystemManager;
+    readonly filePath: string;
+    private _size;
+    constructor(fsm: WechatMiniprogram.FileSystemManager, filePath: string, size: number);
+    size(): number;
+    read(start: number, length: number, success: (params: ArrayBuffer) => void, fail: (e: Error) => void): void;
+    _digest(success: (params: string) => void, fail: (e: Error) => void): void;
+    digest(success: (params: string) => void, fail: (e: Error) => void): void;
+}
+declare class WxmRequest implements IRequest {
+    initRequest(ctx: IContext, params: URLSearchParams, hexHash: ArrayBuffer, resolve: (res: IInitRet) => void, fail: (e: Error) => void): void;
+    chunkRequest(ctx: IContext, params: URLSearchParams, chunk: ArrayBuffer, resolve: (res: IUploadPartRet) => void, fail: (e: Error) => void): void;
+    mergeRequest(ctx: IContext, params: URLSearchParams, chunks: string, resolve: (res: IInitRet) => void, fail: (e: Error) => void): void;
+}
+declare const uploader: (ctx: IContext, filePath: string) => Promise<Uploader<WxmFile, WxmRequest, URLSearchParams>>;
+export default uploader;
